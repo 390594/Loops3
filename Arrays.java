2fff92bfa53f57769b747029df24d657dbfaeb88
Daniel Hoskins <daniel.hoskins@mrjaffesclass.com>
2022-09-23 14:16:08 -0700

    got nothig done lol

--- Arrays.java
+++ Arrays.java
@@ -5,7 +5,7 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class RenameThisClass
+public class Arrays
 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
