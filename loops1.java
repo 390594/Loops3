6b23c430133c4f8a5cb97d9e617245352d884e47
Daniel Hoskins <daniel.hoskins@mrjaffesclass.com>
2022-10-03 14:23:49 -0700

    almost finnished.

--- loops1.java
+++ loops1.java
@@ -2,10 +2,22 @@ public class loops1
 
 {
     public void run() {
-        int i = 0;
+        int i = 5;
+        int k = 15;
+        int a = 0;
+        int b = 10;
         while (i < 15) {
-            System.out.println(" 1 is " + i + " but is less than 15 ");
+            System.out.println("i is" + i + "but is less than 15");
             i += 1;
+        }
+        while (k > 5) {
+            System.out.println("k is" + k + "but is greater than 5");
+            k -= 1;
+        }
+        while (a < 10 && b <= 20) {
+            System.out.println(("a is" + a + "but is less than 10") && ("b is" + b + "but is less than 20");
+            a += 1;
+            b += 1;
         }
     }
 
